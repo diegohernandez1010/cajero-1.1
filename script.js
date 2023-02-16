f1264adc56d4866f1b45b255bc4958a3bdb00bf1
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-02-16 17:12:23 -0500

    avance

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // const nombre =prompt ("seleccione su cuenta");
 // let contraseña = prompt('ingrese su contraseña')
 
-const cuentas=[
-    {nombre: 'mali', saldo: 200, contraseña: 1 },
+const cuentas = [
+    { nombre: 'mali', saldo: 200, contraseña: 1 },
     { nombre: 'gera', saldo: 290, contraseña: 2 },
-    { nombre: 'maui', saldo: 67,  contraseña: 3 }
+    { nombre: 'maui', saldo: 67, contraseña: 3 }
 ];
 
 
@@ -18,195 +18,194 @@ let passwordActual;
 //fin globales.
 
 //datos del usuario, guardar en variables globales.
-function mali(){
-usuarioActual = cuentas[0].nombre;
-saldoActual= cuentas[0].saldo;
-contraseñaActual=cuentas[0].contraseña;
+function mali() {
+    usuarioActual = cuentas[0].nombre;
+    saldoActual = cuentas[0].saldo;
+    contraseñaActual = cuentas[0].contraseña;
 
-// document.getElementById("claveUsuario").style.display = "none";
-//             document.getElementById("opcionesCajero").style.display = "inherit";
+    // document.getElementById("claveUsuario").style.display = "none";
+    //             document.getElementById("opcionesCajero").style.display = "inherit";
 }
 
 
-function gera(){
-usuarioActual= cuentas[1].nombre;
-saldoActual = cuentas[1].saldo;
-contraseñaActual =cuentas[1].contraseña;
-// document.getElementById("claveUsuario").style.display = "none";
-//             document.getElementById("opcionesCajero").style.display = "inherit";
+function gera() {
+    usuarioActual = cuentas[1].nombre;
+    saldoActual = cuentas[1].saldo;
+    contraseñaActual = cuentas[1].contraseña;
+    // document.getElementById("claveUsuario").style.display = "none";
+    //             document.getElementById("opcionesCajero").style.display = "inherit";
 
 }
-function maui(){
+function maui() {
     usuarioActual = cuentas[2].nombre;
-    saldoActual= cuentas[2].saldo;
-    contraseñaActual=cuentas[2].contraseña;
+    saldoActual = cuentas[2].saldo;
+    contraseñaActual = cuentas[2].contraseña;
     // document.getElementById("claveUsuario").style.display = "none";
     //         document.getElementById("opcionesCajero").style.display = "inherit";
 
 }
 // // ocultando opciones cajero
-document.getElementById("opcionesCajero").style.display="none";
+document.getElementById("opcionesCajero").style.display = "none";
 
 //validando usuarios
 
-function validandoUsuario(){ // este es el boton//
-let nombre = document.getElementById("ingreseUsuario").value;//esto es un input
-paso(nombre);
+function validandoUsuario() { // este es el boton//
+    let nombre = document.getElementById("ingreseUsuario").value;//esto es un input
+    paso(nombre);
 
-console.log("nombre usuario= " + nombre);
+    console.log("nombre usuario= " + nombre);
 
 }
-function paso(nombre){         //validando informacion en las cuentas//
-   let usuarioFinal=nombre;
-    for(let i=0; i< cuentas.length; i++){
-if(usuarioFinal == cuentas[i].nombre){
-    usuarioActual= cuentas[i].nombre
- passwordActual=cuentas[i].contraseña
- saldoActual=cuentas[i].saldo
-        // document.getElementById("Usuario").style.display ="none";
-    // document.getElementById("opcionesCajero").style.display = "inherit";
+function paso(nombre) {         //validando informacion en las cuentas//
+    let usuarioFinal = nombre;
+    for (let i = 0; i < cuentas.length; i++) {
+        if (usuarioFinal == cuentas[i].nombre) {
+            usuarioActual = cuentas[i].nombre
+            passwordActual = cuentas[i].contraseña
+            saldoActual = cuentas[i].saldo
+            // document.getElementById("Usuario").style.display ="none";
+            // document.getElementById("opcionesCajero").style.display = "inherit";
+        }
+        //     else if (usuarioFinal==usuarioActual){
+        //         const correcto = document.createTextNode(`ok`);
+        //         const correctoFinal = document.getElementById("correctoUsuario");
+        // correctoFinal.innerHTML ="";
+        // correctoFinal.appendChild (correcto);
+        //   console.log(typeof usuarioFinal);
     }
-//     else if (usuarioFinal==usuarioActual){
-//         const correcto = document.createTextNode(`ok`);
-//         const correctoFinal = document.getElementById("correctoUsuario");
-// correctoFinal.innerHTML ="";
-// correctoFinal.appendChild (correcto);
-    //   console.log(typeof usuarioFinal);
-}
 }
 
 
 //validando contraseñas
 
-function clave(){//este es boton//
+function clave() {//este es boton//
     let contraseña = document.getElementById("password").value;//este es un input//
-    paso2 (contraseña);
+    paso2(contraseña);
 
-    console.log("contraseña= " + contraseña );
-} 
+    console.log("contraseña= " + contraseña);
+}
 
 //se hace la validación. Si está bien, oculta la pantalla de ingreso de clave y muestra las opciones del cajero.
-function paso2(contraseña){
-let validacionFinal= parseInt (contraseña);
+function paso2(contraseña) {
+    let validacionFinal = parseInt(contraseña);
     // for(let i = 0; i < cuentas.length; i++){
-        // if (usuarioActual == cuentas[i].nombre && validacionFinal == cuentas[i].contraseña)
-        if (validacionFinal == passwordActual) {
-                             document.getElementById("clave").style.display = "none";
-                       document.getElementById("Usuario").style.display ="none";
-            document.getElementById("opcionesCajero").style.display = "inherit";
+    // if (usuarioActual == cuentas[i].nombre && validacionFinal == cuentas[i].contraseña)
+    if (validacionFinal == passwordActual) {
+        document.getElementById("clave").style.display = "none";
+        document.getElementById("Usuario").style.display = "none";
+        document.getElementById("opcionesCajero").style.display = "inherit";
 
-        }else {
-            const resultado = document.createTextNode (`Por favor revise su Usuario/Contraseña. Intente nuevamente`);
-            const resultadoError = document.getElementById("errorClave");
-            resultadoError.innerHTML = "";
-            resultadoError.appendChild (resultado);          
-                }
+    } else {
+        const resultado = document.createTextNode(`Por favor revise su Usuario/Contraseña. Intente nuevamente`);
+        const resultadoError = document.getElementById("errorClave");
+        resultadoError.innerHTML = "";
+        resultadoError.appendChild(resultado);
+    }
     // }
 }
 
 //opciones cajero--- ver saldo
 
 document.getElementById("verSaldo").style.display = "none";//ocultar
-const verSaldo=()=>{//boton
-    const saldoFinal=document.createTextNode (`$${saldoActual}`);
-    const mensajeSaldo = document.getElementById ("saldoCliente");
+const verSaldo = () => {//boton
+    const saldoFinal = document.createTextNode(`$${saldoActual}`);
+    const mensajeSaldo = document.getElementById("saldoCliente");
     mensajeSaldo.innerHTML = "";
     mensajeSaldo.appendChild(saldoFinal);
     document.getElementById("verSaldo").style.display = "inherit";
     document.getElementById("opcionesCajero").style.display = "none";
     document.getElementById("verSaldo").style.display = "inherit";
-    }
+}
 
 
 
 
 
 
-const atras=()=>{
+const atras = () => {
     document.getElementById("opcionesCajero").style.display = "inherit";
     document.getElementById("verSaldo").style.display = "none";
 
 
 
 }
-const cerrarSesion=()=>{
+const cerrarSesion = () => {
     document.getElementById("opcionesCajero").style.display = "none";
     document.getElementById("Usuario").style.display = "inherit";
     document.getElementById("clave").style.display = "inherit";
 }
 
-    
+
 //opciones cajero--- agregar saldo
 document.getElementById("agregarSaldo").style.display = "none";
-const ingresarSaldo=()=>{//boton//
-const saldoIngresado = document.getElementById("saldoIngresado").value;//input//
-revisaSaldo (saldoIngresado);
+const ingresarSaldo = () => {//boton//
+    const saldoIngresado = document.getElementById("saldoIngresado").value;//input//
+    revisaSaldo(saldoIngresado);
 
 
     document.getElementById("agregarSaldo").style.display = "inherit";
     document.getElementById("opcionesCajero").style.display = "none";
 
-    document.getElementById("saldoIngresado").value="";
+    document.getElementById("saldoIngresado").value = "";
 
-}    
-
-
+}
 
 
-const atrasSaldo=()=>{
+
+
+const atrasSaldo = () => {
     document.getElementById("opcionesCajero").style.display = "inherit";
     document.getElementById("agregarSaldo").style.display = "none";
     document.getElementById("saldoTotal").style.display = "none";
-    
+
 }
 //ingreso de monto nuevo el agregar saldo
-        const revisaSaldo =(saldoIngresado)=> {
-            let saldoNuevo=(parseInt(saldoIngresado));
-        // let saldoNuevo= (parseInt(saldoIngresado)+parseInt(saldoActual));console.log(saldoNuevo);
-                if(saldoNuevo >990)
-                {
-                const mensajeError = document.createTextNode ("Sobrepasa el monto permitido por el banco. Ingresa un valor inferior.");
-        const mensajeUsuario = document.getElementById ("alertaCupo");
+const revisaSaldo = (saldoIngresado) => {
+    let saldoNuevo = (parseInt(saldoIngresado));
+    // let saldoNuevo= (parseInt(saldoIngresado)+parseInt(saldoActual));console.log(saldoNuevo);
+    if (saldoNuevo > 990) {
+        const mensajeError = document.createTextNode("Sobrepasa el monto permitido por el banco. Ingresa un valor inferior.");
+        const mensajeUsuario = document.getElementById("alertaCupo");
         mensajeUsuario.innerHTML = "";
-        mensajeUsuario.appendChild (mensajeError);
-        
-   
-            }
-        else {
-// document.getElementById("saldoTotal").style.display="none";
+        mensajeUsuario.appendChild(mensajeError);
+
+
+    }
+    else {
+        // document.getElementById("saldoTotal").style.display="none";
         mostrarTransacción(saldoNuevo);
-    
-        }
+
+    }
 
 
-   
+
 }
 
 
-     // Muestra la pantalla con el saldo del cliente
-     
-     document.getElementById("saldoTotal").style.display = "none";
-const mostrarTransacción =(saldoNuevo)=> {
+// Muestra la pantalla con el saldo del cliente
+
+document.getElementById("saldoTotal").style.display = "none";
+const mostrarTransacción = (saldoNuevo) => {
     document.getElementById("agregarSaldo").style.display = "none";
     document.getElementById("saldoTotal").style.display = "none";
 
-     let SaldoActualFinal= (saldoNuevo+saldoActual);
-    
-    const dineroIngresado = document.createTextNode (`$${SaldoActualFinal}`)
-    const mostrarDinero = document.getElementById ("saldoIngresadoCliente");
-    mostrarDinero.innerHTML = "";
-    mostrarDinero.appendChild (dineroIngresado);
+    let SaldoActualFinal = (saldoNuevo + saldoActual);
 
-    if(SaldoActualFinal==SaldoActualFinal){
+    const dineroIngresado = document.createTextNode(`$${SaldoActualFinal}`)
+    const mostrarDinero = document.getElementById("saldoIngresadoCliente");
+    mostrarDinero.innerHTML = "";
+    mostrarDinero.appendChild(dineroIngresado);
+
+    if (SaldoActualFinal == SaldoActualFinal) {
         document.getElementById("saldoTotal").style.display = "inherit";
     }
 
-    if(saldoActual<SaldoActualFinal){
+    if (saldoActual < SaldoActualFinal) {
 
-        saldoActual=SaldoActualFinal;
+        saldoActual = SaldoActualFinal;
     }
-    
-// document.getElementById(saldoNuevo).value=("");
+
+    // document.getElementById(saldoNuevo).value=("");
 
 }
 
